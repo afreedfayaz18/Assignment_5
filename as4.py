7f7d835fd53e4c4ccd890e537d05165674081023
Muhammed Afreed Fayas <79002471+afreedfayaz18@users.noreply.github.com>
2021-03-03 12:28:51 +0530

    Add files via upload

---
+++ as4.py
@@ -0,0 +1,15 @@
+def add(a,b):
+	result=a+b
+	return result
+def sub(a,b):
+	result=a-b
+	return result	
+def mul(a,b):
+	result=a*b
+	return result
+def truediv(a,b):
+	result=a/b
+	return result
+def floordiv(a,b):
+	result=a//b
+	return result			
---
+++ operator.py
@@ -0,0 +1,6 @@
+import operator
+print(operator.add(3,4))
+print(operator.sub(5,2))
+print(operator.mul(9,3))
+print(operator.truediv(20,6))
+print(operator.floordiv(20,6))
