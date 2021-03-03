6d4e0817597f50f23d26afbff4ced2a082f333d6
Muhammed Afreed Fayas <79002471+afreedfayaz18@users.noreply.github.com>
2021-03-03 14:03:51 +0530

    Update as3.py

--- as3.py
+++ as3.py
@@ -1,7 +1,10 @@
-import datetime
-
-now = datetime.datetime.now()
-
-time_string = now.strftime("%Y-%m-%d %H:%M:%S")
-
-print(time_string)
+import time
+time_test=time.strftime('%Y-%m-%d %H:%M:%S%z', time.localtime())
+print (time_test)
+test=time.strptime(time_test, '%Y-%m-%d %H:%M:%S%z')
+print (time.strftime('%Y-%m-%d %H:%M:%S', test))
+l1,l2=[],['yr','mnth','day','hr','min','sec','wkday','yrday','aaa']
+for i in time.localtime():
+	l1.append(i)
+l3=list(zip(l2,l1))
+print(l3)
